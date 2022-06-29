f2004398c0e32a10b22ea73380e7de40b599c99f
RevDonz <aprilio842@gmail.com>
2022-06-29 11:17:34 +0700

    add Register

--- frontend/pages/register.tsx
+++ frontend/pages/register.tsx
@@ -1,42 +1,99 @@
+import { useRouter } from 'next/router';
+import { useForm } from 'react-hook-form';
+import { axiosInstance } from '../lib/axios';
+
+type Register = {
+  name: string;
+  email: string;
+  password: string;
+};
+
 const Register = () => {
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Register>();
+
+  const onSubmit = handleSubmit(async (data) => {
+    console.log(data);
+    const result = await axiosInstance
+      .post('/api/user/register', {
+        ...data,
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    console.log(result?.data.status);
+  });
+
+
   return (
     <div className='h-screen flex items-center justify-center'>
       <div className='card w-96 bg-base-100 shadow-xl'>
         <div className='card-body'>
           <h2 className='card-title text-3xl mb-5'>Register</h2>
-          <form>
-            <div className='form-control w-full max-w-xs'>
+          <form onSubmit={onSubmit}>
+            <div>
               <label className='label'>
                 <span className='label-text'>Nama</span>
+                {errors.name && (
+                  <span className='label-text-alt text-error'>
+                    {errors.name?.message}
+                  </span>
+                )}
               </label>
               <input
                 type='text'
                 placeholder='Type here'
                 className='input input-bordered w-full max-w-xs'
+                {...register('name', { required: 'Name is Required' })}
               />
             </div>
-            <div className='form-control w-full max-w-xs'>
+            <div>
               <label className='label'>
                 <span className='label-text'>Email</span>
+                {errors.email && (
+                  <span className='label-text-alt text-error'>
+                    {errors.email?.message}
+                  </span>
+                )}
               </label>
               <input
                 type='email'
                 placeholder='Type here'
                 className='input input-bordered w-full max-w-xs'
+                {...register('email', {
+                  required: 'Email is Required',
+                  pattern: /^\S+@\S+$/i,
+                })}
               />
             </div>
-            <div className='form-control w-full max-w-xs'>
+            <div>
               <label className='label'>
                 <span className='label-text'>Password</span>
+                {errors.password && (
+                  <span className='label-text-alt text-error'>
+                    {errors.password?.message}
+                  </span>
+                )}
               </label>
               <input
                 type='password'
                 placeholder='Type here'
                 className='input input-bordered w-full max-w-xs'
+                {...register('password', {
+                  required: 'Password is Required',
+                  min: 8,
+                })}
               />
             </div>
             <div className='mt-3 card-actions justify-end'>
-              <button className='btn btn-primary'>Register</button>
+              <button type='submit' className='btn btn-primary'>
+                Register
+              </button>
             </div>
           </form>
           <p>
