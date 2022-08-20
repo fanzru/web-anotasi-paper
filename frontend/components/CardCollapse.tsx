0adb2bb33df0e7a874e214a8d95b6f7c5103f000
RevDonz <aprilio842@gmail.com>
2022-08-20 19:40:18 +0700

    add guidelines

--- frontend/components/CardCollapse.tsx
+++ frontend/components/CardCollapse.tsx
@@ -9,7 +9,7 @@ const CardCollapse: FC<CardCollapse> = ({ title, children }) => {
   return (
     <div className='my-w collapse w-full border rounded-md border-base-300 collapse-arrow mb-6'>
       <input type='checkbox' />
-      <div className='collapse-title text-xl font-medium'>{title}</div>
+      <div className='collapse-title text-xl font-semibold text-blue-500'>{title}</div>
       <div className='collapse-content'>{children}</div>
     </div>
   );
